a69f18929e28fe1d3e61bf98dcfbac3ec29a87bc
jagel <jagel8@hotmail.com>
2022-02-10 13:26:54 -0500

    Nav example

--- src/App.tsx
+++ src/App.tsx
@@ -11,30 +11,77 @@ import {
 import MainApp from './pages/globals/main';
 import Login from './pages/auth/login/login';
 import Dashboard from './pages/globals/dashboard/dashboard';
+import Navbar from 'react-bootstrap/esm/Navbar';
+import { Container } from 'react-bootstrap';
 
- export default function App() {
-   return <div>
-     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/home/dashbaord">Home</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="home/*" element={<MainApp/>}/>
-          <Route path="/login" element={<Login/>} />
-        </Routes>
-      </div>
-    </Router>
+export default function App(){
+  return <>
+  <Navbar bg="light">
+    <Container>
+      <Navbar.Brand href="#home">Brand link</Navbar.Brand>
+    </Container>
+  </Navbar>
+  <br />
+  <Navbar bg="light">
+    <Container>
+      <Navbar.Brand>Brand text</Navbar.Brand>
+    </Container>
+  </Navbar>
+  <br />
+  <Navbar bg="dark">
+  <Container>
+    <Navbar.Brand href="#home">
+      <img
+        src="/logo.svg"
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+        alt="React Bootstrap logo"
+      />
+    </Navbar.Brand>
+  </Container>
+  </Navbar>
+  <br />
+  <Navbar bg="dark" variant="dark">
+    <Container>
+      <Navbar.Brand href="#home">
+        <img
+          alt=""
+          src="/logo.svg"
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{' '}
+      React Bootstrap
+      </Navbar.Brand>
+    </Container>
+  </Navbar>
+</>
+}
+
+//  export default function App() {
+//    return <div>
+//      <Router>
+//       <div>
+//         <nav>
+//           <ul>
+//             <li>
+//               <Link to="/home/dashbaord">Home</Link>
+//             </li>
+//             <li>
+//               <Link to="/login">Login</Link>
+//             </li>
+//           </ul>
+//         </nav>
+//         <Routes>
+//           <Route path="home/*" element={<MainApp/>}/>
+//           <Route path="/login" element={<Login/>} />
+//         </Routes>
+//       </div>
+//     </Router>
     
-   </div>;
- }
+//    </div>;
+//  }
 // import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
