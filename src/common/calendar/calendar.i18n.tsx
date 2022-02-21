43f5439685ff2b2789f5729f59053a0f29a5b6ad
jagel <jagel8@hotmail.com>
2022-02-20 22:24:13 -0500

    Calendar I18n components completed

--- src/common/calendar/calendar.i18n.tsx
+++ src/common/calendar/calendar.i18n.tsx
@@ -1,38 +1,45 @@
-export default function data(){
-
+interface I18n {
+    lang: string;
 }
-// export default class MonthData{
+
+interface dateDataI18n extends I18n {
+    date: Date;
+}
+
+export const I18nSetDaysOfWeek = (props : I18n) => {
+    let daysOfWeek = [];
+
+    switch(props.lang){
+        case 'es':
+            daysOfWeek = ['Do','Lu', 'Ma', 'Mi','Ju','Vi','Sa'];
+        break
     
-//     function DayOfWeedkData(lang:string){
-//         let daysOfWeek: string[] = [];
-//         switch(lang){
-//           case 'es':
-//             daysOfWeek = ['Do','Lu', 'Ma', 'Mi','Ju','Vi','Sa'];
-//           break
-          
-//           default:
-//             daysOfWeek = ['Su','Mo', 'Tu', 'We','Th','Fr','Sa'];
-//           break
-//         }
-//         return daysOfWeek;
-//       }
-      
-//       function GetMonthData(numberMonth:number, lang:string){
-//         let month = [];
-      
-//         switch(lang){
-//           case 'es':
-//             month = ["Enero","Febero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
-//           break
-          
-//           default:
-//             month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-//           break
-//         }
-      
-//           return month[numberMonth];
-//       }
-// }
+        default:
+            daysOfWeek = ['Su','Mo', 'Tu', 'We','Th','Fr','Sa'];
+        break
+    }
+    
+    return  <ul className="calendar-weekdays">
+                {daysOfWeek.map((day) => <li key={day}>{day}</li>)}
+            </ul>
+}
 
+export const  GetMonthAndYearData = (props : dateDataI18n) => {
+    let month : string[] = [];
+    let monthNumber : number = props.date.getMonth();
 
-//https://medium.com/@dan_abramov/smart-and-dumb-components-7ca2f9a7c7d0
+    switch(props.lang){
+        case 'es':
+          month = ["Enero","Febero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
+        break
+        
+        default:
+          month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+        break
+      }
+
+    return   <label>
+    {month[monthNumber]}<br/>
+    {props.date.getFullYear()}
+  </label>
+}
