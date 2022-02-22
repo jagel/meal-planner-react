e17686d299731cc2bbba0cb438fd821a67873f36
jagel <jagel8@hotmail.com>
2022-02-21 21:13:42 -0500

    Create recipe started

---
+++ src/pages/recipes/create/recipe.create.tsx
@@ -0,0 +1,37 @@
+import { Breadcrumb, Button, Container, Form } from "react-bootstrap";
+import Text118n from "../../../common/i18-text";
+
+export default function RecipeCreate(){
+    return <Container>
+        <h1><Text118n text='Create Recipe'></Text118n></h1>
+
+        <Breadcrumb>
+            <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+            <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
+                Library
+            </Breadcrumb.Item>
+            <Breadcrumb.Item active>Data</Breadcrumb.Item>
+        </Breadcrumb>
+
+        <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
+    </Container>
+}
