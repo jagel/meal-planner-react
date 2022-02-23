994eae83ffac99b9fe4e9ca29a70f638c6ece52d
jagel <jagel8@hotmail.com>
2022-02-23 13:31:29 -0500

    Recipe create - prevent default added

--- src/pages/recipes/recipe-create/recipe.create.tsx
+++ src/pages/recipes/recipe-create/recipe.create.tsx
@@ -3,10 +3,14 @@ import { SetLocalizationText } from "../../../utils/i18n/languageManager";
 
 export default function RecipeCreate(){
     const textValue = SetLocalizationText;
+  
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log(event)
+    }
 
     return <Container>
         <h1>{textValue('New Recipe')}</h1>
-
         <Breadcrumb>
             <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
             <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
@@ -15,24 +19,21 @@ export default function RecipeCreate(){
             <Breadcrumb.Item active>Data</Breadcrumb.Item>
         </Breadcrumb>
 
-        <Form>
+        <Form onSubmit={onSubmit}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>{textValue('Name')}</Form.Label>
                 <Form.Control type="text" placeholder={textValue('Recipe Name')} />
-                <Form.Text className="text-muted">
+                {/* <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
-                </Form.Text>
+                </Form.Text> */}
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
+                <Form.Label>{textValue('Recipe Description')}</Form.Label>
+                <Form.Control type="text" placeholder={textValue('Recipe Description')} />
             </Form.Group>
             <Button variant="primary" type="submit">
-                Submit
+                {textValue('save')}
             </Button>
         </Form>
     </Container>
