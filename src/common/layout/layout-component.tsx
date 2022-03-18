d20cd2172fa70f1e9dc3ab99ea8832cc1eb25c18
jagel <jagel8@hotmail.com>
2022-03-18 14:18:58 -0400

    Loading style removed

--- src/common/layout/layout-component.tsx
+++ src/common/layout/layout-component.tsx
@@ -2,16 +2,12 @@ import React from "react"
 import Container from "react-bootstrap/esm/Container"
 import { Outlet } from "react-router-dom"
 import NavBar from "../../components/navigation/nav-bar/nav-bar"
-import { useAppContext } from "../app/app-context"
 import { Authentication } from "../app/authentication"
 import './loading.css';
 
 export const Layout = () => {
-    let appContext = useAppContext();
-
 return <Authentication>
     <React.Suspense fallback={<>...</>}> 
-     <div className={appContext.appManager.userValidated ? "loading": ""} ></div>
     <NavBar/>
     <Container>
         <Outlet />
