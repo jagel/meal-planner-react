2c72b7736448d20615e2b765b0461b344892ca50
jagel <jagel8@hotmail.com>
2022-03-29 20:55:48 -0400

    navigation bar updated using mui

--- src/common/app/google.icon.tsx
+++ src/common/app/google.icon.tsx
@@ -5,10 +5,13 @@ export const Icons = {
     account_circle: 'account_circle',
     language:'language',
     menu: 'menu',
+    search: 'search',
+    notifications:'notifications',
+    morevert:'more_vert'
 }
 
 export const GoogleIconsInheritance = (props:{iconName:string}) =>
-    <span className="lng-icon material-icons">{props.iconName}</span>
+    <span className="material-icons">{props.iconName}</span>
 
 export const GoogleIconComposition = (props : {iconName:string, children : React.ReactNode}) =>
-  <span className="lng-icon material-icons">{props.iconName}{props.children}</span>
+  <span className="material-icons">{props.iconName}{props.children}</span>
