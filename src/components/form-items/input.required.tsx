43b17f0267f5190b26c57d6bb4f6fc3fc6941337
jagel <jagel8@hotmail.com>
2022-03-30 22:02:06 -0400

    input item implemented

---
+++ src/components/form-items/input.required.tsx
@@ -0,0 +1,30 @@
+import { SetLanguageText } from "../../services/i18n/languageManager";
+import { FormValidationservice } from "../../services/form.validation.service";
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+
+export const InputRequired = (props :{
+    value:string,
+    onTextChange(event : React.ChangeEvent<HTMLInputElement>):void,
+    displayError:boolean,
+    name:string,
+    displayText:string
+  }) => {
+    
+    const displayName = SetLanguageText(props.displayText);
+    const errorMessage = SetLanguageText('required field');
+    const displayError =  FormValidationservice.displayRequiredError(props.value, props.displayError);;
+    const helperText = displayError ? errorMessage  : '';
+  
+    return <FormControl fullWidth sx={{ m: 1 }} >
+       <TextField required
+          error={displayError} 
+          id={props.name}
+          label={displayName}
+          defaultValue={props.value}
+          helperText={helperText}
+          onChange={props.onTextChange}
+          variant="standard"
+        />
+    </FormControl>
+  }
