1cea2c8ec393dffca3e645d1c316a11d9d70785a
jagel <jagel8@hotmail.com>
2022-04-04 12:59:15 -0400

    Update recipe updated

--- src/pages/recipes/recipe.update.tsx
+++ src/pages/recipes/recipe.update.tsx
@@ -1,79 +1,95 @@
-import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { IRecipeModel, StepModel } from "../../common/models/recipe.form";
+import { RecipeModel, StepModel } from "../../common/models/recipe.form";
 import { RecipeForm, RecipeFormProps } from "../../components/recipes/recipe.form";
 import { recipeEndpointsService } from "../../services/endpoints/recipe.enpoints.service";
 import { LayoutPage } from "../../common/layout/layout-page";
-import { ButtonLoading } from "../../common/buttonLoader/button.loader";
+import { ButtonLoading, ButtonLoadingProp } from "../../common/buttonLoader/button.loader";
 import { RoutingServices } from "../../services/routing.service";
 import { APP_ROUTES } from "../../utils/routing/app-routes";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { FormModel } from "../../common/models/form-model";
 
 export default function RecipeUpdate(){
-  const [validated, setValidated] = useState(false);
   const [initialLoading, setInitialLoading] = useState(true);
-  const { recipeId } = useParams();
-
-  const [recipeForm, setRecipeFormState] = useState({} as IRecipeModel);
+  const [editionModel, setEditionMode] = useState(false);
+  const [recipeForm, setRecipeFormState] = useState(new FormModel<RecipeModel>(new RecipeModel()));
+  
   const routingService = RoutingServices;
   const navigate = useNavigate();
 
+  const { recipeId } = useParams();
+
   useEffect(()=>{
-      recipeEndpointsService.getRecipeAsync(recipeId??'')
-          .then(response => {
-            setRecipeFormState(response??recipeForm);
-            setInitialLoading(false);
-          });
+    recipeEndpointsService.getRecipeAsync(recipeId??'')
+        .then(response => {
+          setRecipeFormState(prevState => ({...prevState, model: response??recipeForm.model}) );
+          setInitialLoading(false);
+        });
   },[])
 
   const handleSubmit = (event:React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     event.stopPropagation();
-    
-  const form = event.currentTarget;
-  let isValid = form.checkValidity();
 
-  if (isValid) 
-    recipeEndpointsService.updateRecipeAsync(recipeForm,recipeId??'')
-      .then( response => {
-        let route = routingService.generateRoute(APP_ROUTES.RECIPES_VIEW, {recipeId:response.recipeId});
-        navigate(route);
-      });
-  else
-    setValidated(true);      
-};  
+    const isValid = event.currentTarget.checkValidity();
+
+    if (isValid) {
+      let recipeResponse :RecipeModel;
+      setLoading(true);
+      recipeEndpointsService.updateRecipeAsync(recipeForm.model , recipeId??'')
+        .then( modelSaved => recipeResponse = modelSaved)
+        .finally(() => {
+          setLoading(false);
+          if(recipeResponse !== undefined) {
+            let route = routingService.generateRoute(APP_ROUTES.RECIPES_VIEW, {recipeId:recipeResponse.recipeId});
+            navigate(route);
+          }
+        });
+    }
+
+      else {
+        setDisplayErrors(true);
+      }
+  };
+
+  const setLoading = (loading : boolean) => {
+    setRecipeFormState(prevState => ({
+      ...prevState,
+      isLoading : loading
+      })
+    );
+  }
+
+  const onTextChange = (event : React.ChangeEvent<HTMLInputElement>) => 
+    setRecipeFormState(prevState => ({ 
+        ...prevState, 
+        model:{...prevState.model, [event.target.id] : event.target.value } 
+      })
+    );;
+
+  const updateSteps = (steps : StepModel[] = []) =>  
+    setRecipeFormState(prevState => ({ ...prevState,
+        model:{ ...prevState.model, steps: steps }
+      })
+    );
 
 
-const onTextChange = (event : React.ChangeEvent<HTMLInputElement>) =>{
-  setRecipeFormState({
-      ...recipeForm,
-      [event.target.id]: event.target.value
-    });
-}
 
-const updateSteps = (steps : StepModel[]) => {
-  setRecipeFormState({
-    ...recipeForm,
-    steps: steps
-  });
-}
+  const setDisplayErrors = (displayError : boolean) => {
+    setRecipeFormState(prevState => ({
+      ...prevState,
+      displayErrors : displayError
+      })
+    );
+  } 
 
-// const formPoperties : RecipeFormProps = {
-//   recipeForm,
-//   onTextChange,
-//   updateSteps,
-//   onEditionModel:(demo:boolean) => console.log('edition'),
-// }
+  const formPoperties : RecipeFormProps = { recipeForm, onTextChange, updateSteps, onEditionModel:setEditionMode };
+  const buttonProperties : ButtonLoadingProp = { text:"save", fullWidth:false, loading:recipeForm.isLoading||editionModel };
   
-  return  <LayoutPage params={{recipeId}} loadingPage={initialLoading}>
-      <form onSubmit={handleSubmit} noValidate >
-        {/* <RecipeForm 
-          recipe={recipeForm} 
-          onTextChange={onTextChange} 
-          displayError={validated}
-          updateSteps={updateSteps}
-        /> */}
-        <ButtonLoading text="update" fullWidth={false} loading={false} />
-      </form>  
-</LayoutPage>
+  return <LayoutPage params={{recipeId}} loadingPage={initialLoading}>
+    <form onSubmit={handleSubmit} noValidate >
+      <RecipeForm {...formPoperties} />
+      <ButtonLoading {...buttonProperties} />
+    </form>
+  </LayoutPage>
 }
