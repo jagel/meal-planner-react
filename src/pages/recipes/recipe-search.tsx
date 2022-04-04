dc65e3f67eb6afcbc5544a2852ab9ceac0e8fcfb
jagel <jagel8@hotmail.com>
2022-04-04 13:07:41 -0400

    #30 Search page cleaned

--- src/pages/recipes/recipe-search.tsx
+++ src/pages/recipes/recipe-search.tsx
@@ -1,14 +1,11 @@
 import { SetLanguageText } from "../../services/i18n/languageManager";
 import { BreadcrumbRoutes } from "../../components/navigation/breadcrumb-routes";
+import { LayoutPage } from "../../common/layout/layout-page";
 
 export default function RecipeSearch(){
     const textValue = SetLanguageText;
 
-    return <div>
-        <BreadcrumbRoutes />
-
-        <div>
-            
-        </div>
-    </div>
+    return <LayoutPage>
+        <label>search cleaned</label>    
+  </LayoutPage>
 }
