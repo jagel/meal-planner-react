fb6ae67955c0359eda50fbe3eede52531aef6020
jagel <jagel8@hotmail.com>
2022-05-04 12:41:28 -0400

    Error message display updated

--- src/common/app/error-message.tsx
+++ src/common/app/error-message.tsx
@@ -1,18 +1,15 @@
+import Alert from "@mui/material/Alert"
+import AlertTitle from "@mui/material/AlertTitle"
 import { ErrorObject } from "../../services/endpoints/error.handler"
 
 export const ErrorMessage = (props : {errors?:ErrorObject|undefined}) => {
-    return props.errors ? <div>
-        <label>Error Response</label>
-        <ul>
-        <li>Title: {props.errors?.title}</li>
-        <li>DisplayType: {props.errors?.displayType}</li>
-        <li>Details: {props.errors?.details}</li>
-        <li>Errors:
-            <ul>
-                {props.errors?.errors?.map(item => <li key={item.title}>Title: {item.title}, Description: {item.description}</li>)}
-            </ul>
-        </li>
+    return props.errors ? <Alert severity="warning" style={{marginTop:'10px'}}>
+    <AlertTitle>{props.errors?.title}</AlertTitle>
+    <p>{props.errors?.details} </p>
+    <ul>
+        {props.errors?.errors?.map(item => <li key={item.title}><strong>{item.title}</strong>: {item.description}</li>)}
     </ul>
-    </div>:
-    <div></div>
+  </Alert>
+   :
+    <></>
 }
