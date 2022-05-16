a9e86c19e9d0faf6eb7560b813a36ba455d87753
jagel <jagel8@hotmail.com>
2022-05-16 17:06:52 -0400

    Calendar updated

--- src/common/calendar/calendar.i18n.tsx
+++ src/common/calendar/calendar.i18n.tsx
@@ -1,42 +1,54 @@
-interface I18n {
-    lang: string;
+import { LANG_DATA } from "../../utils/data/languageAvailable";
+import { useAppContext } from "../app/app-context";
+
+interface DaysOfWeekProps {
+  disableDays?:Array<number>
 }
 
-interface dateDataI18n extends I18n {
-    date: Date;
+export const DaysOfWeek = (props: DaysOfWeekProps) => {
+  let appContext = useAppContext();
+  let daysOfWeek = [];
+
+  switch(appContext.language){
+      case LANG_DATA.ES:
+          daysOfWeek = ['Do','Lu', 'Ma', 'Mi','Ju','Vi','Sa'];
+      break;
+  
+      case LANG_DATA.EN:
+          daysOfWeek = ['Su','Mo', 'Tu', 'We','Th','Fr','Sa'];
+      break;
+
+      default:
+        console.warn(`Language: ${appContext.language} does not exist`);
+        return <></>;
+  }
+  
+  return  <ul className="calendar-weekdays">
+    {daysOfWeek.map((day,index) => <li key={day} className={props.disableDays?.includes(index)? 'day-disabled': 'day-available'}>{day}</li>)}
+  </ul>
 }
 
-export const I18nSetDaysOfWeek = (props : I18n) => {
-    let daysOfWeek = [];
 
-    switch(props.lang){
-        case 'es':
-            daysOfWeek = ['Do','Lu', 'Ma', 'Mi','Ju','Vi','Sa'];
-        break
-    
-        default:
-            daysOfWeek = ['Su','Mo', 'Tu', 'We','Th','Fr','Sa'];
-        break
+interface GetMonthAndYearDataProps {
+  date: Date;
+}
+export const  GetMonthAndYearData = (props : GetMonthAndYearDataProps) => {
+  let appContext = useAppContext();
+  let month : string[] = [];
+  let monthNumber : number = props.date.getMonth();
+
+  switch(appContext.language){
+      case LANG_DATA.ES:
+        month = ["Enero","Febero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
+      break;
+      
+      case LANG_DATA.EN:
+        month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+      break;
+      default:
+        console.warn(`Language: ${appContext.language} does not exist`);
+      return <></>;
     }
-    
-    return  <ul className="calendar-weekdays">
-                {daysOfWeek.map((day) => <li key={day}>{day}</li>)}
-            </ul>
-}
-
-export const  GetMonthAndYearData = (props : dateDataI18n) => {
-    let month : string[] = [];
-    let monthNumber : number = props.date.getMonth();
-
-    switch(props.lang){
-        case 'es':
-          month = ["Enero","Febero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
-        break
-        
-        default:
-          month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-        break
-      }
 
     return   <label>
     {month[monthNumber]}<br/>
--- src/common/calendar/calendar.tsx
+++ src/common/calendar/calendar.tsx
@@ -2,13 +2,17 @@
 
 import './calendar.css'
 import React, { useState } from 'react';
-import { GetMonthAndYearData, I18nSetDaysOfWeek } from './calendar.i18n';
+import { DaysOfWeek, GetMonthAndYearData } from './calendar.i18n';
+import { CalendarDays, CalendarDaysProps } from './calendarDays';
 
 let dateSelected = new Date();
 let currentDateData = new Date();
 
-export default function JGLCalendar(props:any){
-  let lang = props.lang??'es';
+export interface JGLCalendarProps {
+  disableDays?:Array<number>
+}
+
+export default function JGLCalendar(props:JGLCalendarProps){
   const [dateData, setDateData] = useState({current:currentDateData, selected:dateSelected});
 
   const changeMonth = (numberOfMonths:number) => {
@@ -21,59 +25,16 @@ export default function JGLCalendar(props:any){
     setDateData({...dateData, current:dateData.current, selected:dateSelected})
   }
 
+  const calendarDaysProps : CalendarDaysProps = {current:dateData.current, selected: dateData.selected, onDayClick, disableDays:[0,6]}
   return <div>
     <div className='calendar-month'>
       <div className='page' onClick={()=>changeMonth(-1)}><label>&#10094;</label></div>
       <div className='month-name'>
-        <GetMonthAndYearData lang={lang} date={dateData.current} />
+        <GetMonthAndYearData date={dateData.current} />
       </div>
       <div className='page' onClick={()=>changeMonth(1)}><label>&#10095;</label></div>
     </div>
-    <I18nSetDaysOfWeek lang={lang} />
-    <SetCalendarDays current={dateData.current} selected={dateData.selected} onDayClick={onDayClick}  />
+    <DaysOfWeek disableDays={[0,6]} />
+    <CalendarDays {...calendarDaysProps} />
   </div>
 }
-
-
-const SetCalendarDays = (props: {current:Date, selected:Date, onDayClick(day:number): void}) => {
-  const getDateSplit = (date : Date) : {day:number, month: number, year: number} => {return {day:date.getDate(),month:date.getMonth(), year:date.getFullYear()}};
-  let currentDate = getDateSplit(props.current);
-  let selectedDate = getDateSplit(props.selected);
-
-  const getFisrtEmpySpaces = () : number => {
-    let firstDayMonth = new Date(currentDate.year , currentDate.month , 1);
-    let emptyItems = firstDayMonth.getDay();
-    return emptyItems;
-  }
-
-  const getLastEmpySpacesAndLastDate = () : {lastDay : number, lastEmptyItems:number } => {
-    let lastDayMonth = new Date(currentDate.year , (currentDate.month + 1) , 1 );
-    lastDayMonth.setDate(lastDayMonth.getDate()-1);
-    let lastEmptyDays = 6 - lastDayMonth.getDay();
-    let lastDay = lastDayMonth.getDate();
-    return {lastDay : lastDay, lastEmptyItems: lastEmptyDays };
-  }
-  
-  const getClassNamme = (day : number)=>{
-    day++;
-    let className = '';
-    if(currentDate.month === selectedDate.month && currentDate.year === selectedDate.year && selectedDate.day === day)
-      className = 'active';
-
-    return className;
-  }
-
-  let firstEmptyItems = getFisrtEmpySpaces();
-  let endMonthData = getLastEmpySpacesAndLastDate();
-
-  const populateEmptyListItems = (numberOfItems : number) => Array.apply(0, Array(numberOfItems)).map((x:any, i:number) => <li key={i}></li>);
-
-  return <ul className='calendar-days'>
-    {populateEmptyListItems(firstEmptyItems)}
-    {Array.apply(0, Array(endMonthData.lastDay)).map(function (x, i) {
-      return <li key={i} className={getClassNamme(i)} onClick={()=>props.onDayClick(i+1)}>{i+1}</li>;
-    })}
-    {populateEmptyListItems(endMonthData.lastEmptyItems)}
-  </ul>
-}
-
