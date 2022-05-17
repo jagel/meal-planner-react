76377206511c25833c30c50c15f7d2849f117f30
jagel <jagel8@hotmail.com>
2022-05-16 21:19:11 -0400

    Agenda meal planner added

---
+++ src/pages/agenda/agenda-meal-planner.tsx
@@ -0,0 +1,10 @@
+import Grid from "@mui/material/Grid"
+import { LayoutPage } from "../../common/layout/layout-page"
+
+export const AgendaMealPlanner = () => {
+    return  <LayoutPage>
+        <Grid>
+            <label>demo</label>
+        </Grid>
+    </LayoutPage>
+}
