7abfc4ef1256104ce1435a48814dc8b11938be37
jagel <jagel8@hotmail.com>
2022-05-23 12:14:54 -0400

    Removed subtask variable from global form model

--- src/common/models/form-model.tsx
+++ src/common/models/form-model.tsx
@@ -1,7 +1,6 @@
 export class FormModel<ModelForm>{
     isLoading:boolean = false;
     displayErrors:boolean = false;
-    onSubtask:boolean = false;
     model : ModelForm;
     
     constructor(initializeModel:ModelForm){
