0aa4dd3847d0b55b75fdcc5495b08c128fc09e77
jagel <jagel8@hotmail.com>
2022-05-24 10:19:27 -0400

    skeleton loading added as optional in global model form

--- src/common/models/form-model.tsx
+++ src/common/models/form-model.tsx
@@ -2,17 +2,21 @@ export class FormModel<ModelForm>{
     isLoading:boolean = false;
     displayErrors:boolean = false;
     model : ModelForm;
+    skeletonLoading?:boolean;
 
     constructor(model: ModelForm, props:{
         isLoading?:boolean,
         displayErrors?:boolean,
+        skeletonLoading?:boolean
     }={
         isLoading : false,
         displayErrors : false,
+        skeletonLoading:false,
     }) {
         this.model = model;
         this.isLoading = props.isLoading??false;
         this.displayErrors = props.displayErrors??false;
+        this.skeletonLoading = props.skeletonLoading??false
     }
-
+    
 }
